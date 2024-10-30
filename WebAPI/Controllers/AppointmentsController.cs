5232a22b3725e754b20b9ef731baae15e7b73173
Kerem Kenan TEKER <tekerkerem@gmail.com>
2024-10-30 17:04:29 +0300

    api

---
+++ WebAPI/Controllers/AppointmentsController.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntitiyFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentsController : ControllerBase
+    {
+
+        //Loosely coupled yani bir bağımlılığı var ama soyuta bağlı (gevşek bağlılık)
+        //dependency injectionda ctor içindeki appointmentManager a zaten erişemeyiz fielda eşitleriz onu kullanırız. Bu desenin bir yönü de bu.
+        //IOC Containerr -- Inversion of Control
+        IAppointmentService _appointmentService;
+        public AppointmentsController(IAppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService;
+        }
+
+        [HttpGet]
+        public List<Appointment> Get()
+        {
+            //AppointmentManager bizden hep bir Ef istiyor
+            //sadece datayı istiryoruz mesaj ve success de döndürebilirdik.
+            //Dependecy Chain -- IAppointmentService --> AppointmentManager --> EfAppointmentDal
+            //IAppointmentService appointmentService = new AppointmentManager(new EfAppointmentDal());
+            //var result = appointmentService.GetAll();
+            //apide hiç bir business veya dal somut sınıfımız yok.
+
+            //böyle yaptığımızda bağımsız oluyor ama controllerın genede somut bir referansa ihtiyacı var çünkü hangi IAppointmentService olduğunu bilemez.
+            //bu durumda IOC Container a ihtiyacımız var.
+            //referansları IOC Containerda ayrı olarak veririz. Api bizim yerimize gider verilen referanslara bakar.
+            var result = _appointmentService.GetAll();
+            return result.Data;
+            
+        }
+    }
+}
---
+++ WebAPI/Program.cs
@@ -0,0 +1,34 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntitiyFramework;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// IoC kayýtlarý buraya eklenir.
+builder.Services.AddSingleton<IAppointmentService, AppointmentManager>();
+builder.Services.AddSingleton<IAppointmentDal, EfAppointmentDal>();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
