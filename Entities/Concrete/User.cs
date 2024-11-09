11948bf0d26b79d4b667cdeb146907b7c16c08b5
Kerem Kenan TEKER <tekerkerem@gmail.com>
2024-11-09 15:05:22 +0300

    appsettings.json

--- Entities/Concrete/User.cs
+++ Entities/Concrete/User.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace Entities.Concrete
 {
     public class User : IEntity
@@ -13,8 +15,10 @@ namespace Entities.Concrete
         public string UserName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string Password { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
         public string NotificationPreferences { get; set; }
+        
 
         public ICollection<Notification> Notifications { get; set; }
         public ICollection<PaymentTransaction> PaymentTransactions { get; set; }
